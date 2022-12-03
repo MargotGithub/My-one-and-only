f9e638fc12fadfd61db42f1dd0d7eaeb1efc969b
MargotGithub <novakova.kristynaa@gmail.com>
2022-12-03 22:28:30 +0100

    added function to fetch temperature and description

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+}
+
 let apiKey = "03cb34007tb73o9aaf043209471b5355";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Sydney&key=${apiKey}&units=metric
 `;
 console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
