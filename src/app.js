84a1cc99a259411d1da39ed6d2e199d618383daf
MargotGithub <novakova.kristynaa@gmail.com>
2022-12-11 23:28:25 +0100

    added search engine and temp conversion

--- src/app.js
+++ src/app.js
@@ -39,7 +39,10 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  celsiusTemp = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -52,9 +55,42 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
-let apiKey = "24b5598b1118ec419d9b4f4bdb9a5cbd";
-let city = "Stuttgart";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=24b5598b1118ec419d9b4f4bdb9a5cbd&units=metric
+function submit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+function search(city) {
+  let apiKey = "24b5598b1118ec419d9b4f4bdb9a5cbd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric
 `;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitConversion = Math.round((celsiusTemp * 9) / 5 + 32);
+
+  temperatureElement.innerHTML = fahrenheitConversion;
+}
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
+
+search("Prague");
